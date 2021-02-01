9a949d54bce578ad3000927ce97a3924871ba73a
Neelesh Mishra <neeleshojmishra@gmail.com>
2021-02-01 14:33:43 +0530

    Updating examples

---
+++ examples/xor/xor.py
@@ -0,0 +1,21 @@
+from neatpy.options import Options
+from neatpy.population import Population
+
+xor_inp = [(0,0), (0,1), (1,0), (1,1)]
+xor_out = [0, 1, 1, 0]
+
+def evaluate(nns):
+    for nn in nns:
+        nn.fitness = 4
+    
+        for xi, xo in zip(xor_inp, xor_out):
+            output = nn.predict(xi)[0]
+            nn.fitness -= (output - xo) ** 2
+
+Options.set_options(2, 1, 150, 3.9)
+
+p = Population()
+best, solved = p.evaluate(evaluate, 400)
+
+evaluate([best])
+print(best.fitness)
