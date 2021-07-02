3db371c8897bf1f2048b89e9da8fe3ce4317bcda
ai_boy <neeleshojmishra@gmail.com>
2021-07-02 16:24:01 +0530

    Refactor tests

--- neat_new/neat.py
+++ neat_new/neat.py
@@ -1,12 +1,97 @@
 import random
 
 
-class Options:
-    inputs = None
-    outputs = None
+def sigmoid(x):
+    try:
+        return (1.0 / (1.0 + math.exp(-x)))
+    except OverflowError:
+        return 0 if x < 0 else 1
 
-    add_node_prob = 0.02
-    add_conn_prob = 0.05
+
+class Options:
+    @staticmethod
+    def set_options(
+        inputs,
+        outputs,
+        population_size=150,
+
+        fitness_threshold=float('inf'),
+        max_nodes=float('inf'),
+
+        activation_func=sigmoid,
+        aggregation_func=sum,
+
+        excess_coeff=1,
+        disjoint_coeff=1,
+        weight_coeff=0.5,
+
+        add_node_prob=0.02,
+        add_conn_prob=0.05,
+
+        weight_mutate_prob=0.1,
+        new_weight_prob=0.1,
+        weight_init_range=1,
+        weight_mutate_power=0.5,
+
+        feature_selection=False,
+
+        compatibility_threshold=3,
+        dynamic_compatibility_threshold=True,
+
+        target_species=20,
+        dropoff_age=15,
+        survival_rate=0.2,
+        species_elitism=True,
+
+        crossover_rate=1,
+        tries_tournament_selection=3,
+
+        young_age_threshhold=10,
+        young_age_fitness_bonus=1.3,
+        old_age_threshold=50,
+        old_age_fitness_penalty=0.7
+    ):
+        Options.inputs = inputs
+        Options.outputs = outputs
+        Options.population_size = population_size
+
+        Options.fitness_threshold = fitness_threshold
+        Options.max_nodes = max_nodes
+
+        Options.activation_func = activation_func
+        Options.aggregation_func = aggregation_func
+
+        Options.excess_coeff = excess_coeff
+        Options.disjoint_coeff = disjoint_coeff
+        Options.weight_coeff = weight_coeff
+
+        Options.add_node_prob = add_node_prob
+        Options.add_conn_prob = add_conn_prob
+
+        Options.weight_mutate_prob = weight_mutate_prob
+        Options.new_weight_prob = new_weight_prob
+        Options.weight_init_range = weight_init_range
+        Options.weight_mutate_power = weight_mutate_power
+
+        Options.feature_selection = feature_selection
+
+        Options.compatibility_threshold = compatibility_threshold
+        Options.dynamic_compatibility_threshold = dynamic_compatibility_threshold
+
+        Options.target_species = target_species
+        Options.dropoff_age = dropoff_age
+        Options.survival_rate = survival_rate
+        Options.species_elitism = species_elitism
+
+        Options.crossover_rate = crossover_rate
+        Options.tries_tournament_selection = tries_tournament_selection
+
+        Options.young_age_threshhold = young_age_threshhold
+        Options.young_age_fitness_bonus = young_age_fitness_bonus
+        Options.old_age_threshold = old_age_threshold
+        Options.old_age_fitness_penalty = old_age_fitness_penalty
+
+        Node.setup()
 
 
 class NodeState:
@@ -23,6 +108,7 @@ class Node:
 
     @staticmethod
     def setup():
+        Node.pos = {}
         Node.history = {}
         Node.node_id = Options.inputs + Options.outputs + 1
 
@@ -37,6 +123,12 @@ class Node:
         if Node.history.get(conn) is None:
             Node.history[conn] = Node.node_id
             Node.node_id += 1
+
+            p1 = Node.pos[conn[0]]
+            p2 = Node.pos[conn[1]]
+
+            Node.pos[Node.history[conn]] = (
+                p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2
 
         return Node.history[conn]
 
--- neat_new/tests.py
+++ neat_new/tests.py
@@ -2,72 +2,47 @@ import random
 from neat import Brain, Options, Node
 
 
-class TestGen:
-    def check_num(self, i, o, b):
-        assert len(b._nodes) == i + o + 1
-        assert len(b._conns) == (i + 1) * o
-
-    def check_valid(self, i, o, b):
-        for node_id in range(i + o + 1):
-            assert node_id in b._nodes
-
-        for conn in list(b._conns):
-            assert 0 <= conn[0] <= i
-            assert i < conn[1] <= i + o
-
-    def test_main(self):
+def brain_test(func):
+    def wrap():
         for _ in range(1000):
             i, o = random.randrange(1, 20), random.randrange(1, 20)
 
-            Options.inputs = i
-            Options.outputs = o
-            Node.setup()
-
+            Options.set_options(i, o)
             b = Brain()
 
-            self.check_num(i, o, b)
-            self.check_valid(i, o, b)
+            func(i, o, b)
+
+    return wrap
 
 
-class TestNode:
-    def check_add_node(self, b):
-        n1 = b._nodes.copy()
-        b._add_node()
-        n2 = b._nodes.copy()
+@brain_test
+def test_main(i, o, b):
+    assert len(b._nodes) == i + o + 1
+    assert len(b._conns) == (i + 1) * o
 
-        node_id = list(n2 - n1)[0]
-        conn = None
+    for node_id in range(i + o + 1):
+        assert node_id in b._nodes
 
-        for c in Node.history:
-            if Node.history[c] == node_id:
-                conn = c
+    for conn in list(b._conns):
+        assert 0 <= conn[0] <= i
+        assert i < conn[1] <= i + o
 
-        assert b._conns[conn]['enabled'] == False
-        assert b._conns.get((conn[0], node_id)) is not None
-        assert b._conns.get((node_id, conn[1])) is not None
-        assert len(n1) + 1 == len(n2)
 
-    def check_add_conn(self, b):
-        pass
+@brain_test
+def test_add_node(i, o, b):
+    n1 = b._nodes.copy()
+    b._add_node()
+    n2 = b._nodes.copy()
 
-    def test_main(self):
-        for _ in range(1000):
-            i, o = random.randrange(1, 5), random.randrange(1, 5)
+    node_id = list(n2 - n1)[0]
+    conn = None
+    for c in Node.history:
+        if Node.history[c] == node_id:
+            conn = c
 
-            Options.inputs = i
-            Options.outputs = o
+    assert b._conns[conn]['enabled'] == False
 
-            Node.setup()
-            b = Brain()
+    assert b._conns.get((conn[0], node_id)) is not None
+    assert b._conns.get((node_id, conn[1])) is not None
 
-            self.check_add_node(b)
-
-        for _ in range(1000):
-            i, o = random.randrange(1, 5), random.randrange(1, 5)
-            Node.setup()
-
-            Options.inputs = i
-            Options.outputs = o
-            b = Brain()
-
-            self.check_add_conn(b)
+    assert len(n1) + 1 == len(n2)
