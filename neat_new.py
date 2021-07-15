476dbc4da529448f859a6096ba8178b2657c0d7a
ai_boy <neeleshojmishra@gmail.com>
2021-07-15 12:39:24 +0530

    Refactor Brain distance

--- neat_new.py
+++ neat_new.py
@@ -1,5 +1,4 @@
 import random
-import enum
 import math
 import copy
 
@@ -250,9 +249,6 @@ class Brain:
                         )
                     )
 
-    def _filter_nodes(self, *args):
-        return [node for node in self.nodes if Node.get_state(node.id) in args]
-
     def _add_conn(self):
         valid = []
 
@@ -471,6 +467,53 @@ class Brain:
 
         return Brain(baby_id, baby_nodes, baby_connections)
 
+    @staticmethod
+    def distance(genome1, genome2):
+        n_match = n_disjoint = n_excess = 0
+        weight_difference = 0
+
+        n_g1 = len(genome1.connections)
+        n_g2 = len(genome2.connections)
+        i_g1 = i_g2 = 0
+
+        while i_g1 < n_g1 or i_g2 < n_g2:
+            # excess
+            if i_g1 == n_g1:
+                n_excess += 1
+                i_g2 += 1
+                continue
+
+            if i_g2 == n_g2:
+                n_excess += 1
+                i_g1 += 1
+                continue
+
+            conn1 = genome1.connections[i_g1]
+            conn2 = genome2.connections[i_g2]
+
+            # match
+            if conn1.innov == conn2.innov:
+                n_match += 1
+                i_g1 += 1
+                i_g2 += 1
+                weight_difference += abs(conn1.weight-conn2.weight)
+                continue
+
+            # disjoint
+            if conn1.innov < conn2.innov:
+                n_disjoint += 1
+                i_g1 += 1
+                continue
+
+            if conn1.innov > conn2.innov:
+                n_disjoint += 1
+                i_g2 += 1
+                continue
+
+        n_match += 1
+        return (Options.excess_coeff * n_excess + Options.disjoint_coeff * n_disjoint) / max(n_g1, n_g2) + Options.weight_coeff * weight_difference / n_match
+
+
 
 class Species:
     def __init__(self, species_id, member):
@@ -528,54 +571,8 @@ class Species:
             self.stagnation = 0
             self.max_fitness = self.best.fitness
 
-    @staticmethod
-    def compat_dist(genome1, genome2):
-        n_match = n_disjoint = n_excess = 0
-        weight_difference = 0
-
-        n_g1 = len(genome1.connections)
-        n_g2 = len(genome2.connections)
-        i_g1 = i_g2 = 0
-
-        while i_g1 < n_g1 or i_g2 < n_g2:
-            # excess
-            if i_g1 == n_g1:
-                n_excess += 1
-                i_g2 += 1
-                continue
-
-            if i_g2 == n_g2:
-                n_excess += 1
-                i_g1 += 1
-                continue
-
-            conn1 = genome1.connections[i_g1]
-            conn2 = genome2.connections[i_g2]
-
-            # match
-            if conn1.innov == conn2.innov:
-                n_match += 1
-                i_g1 += 1
-                i_g2 += 1
-                weight_difference += abs(conn1.weight-conn2.weight)
-                continue
-
-            # disjoint
-            if conn1.innov < conn2.innov:
-                n_disjoint += 1
-                i_g1 += 1
-                continue
-
-            if conn1.innov > conn2.innov:
-                n_disjoint += 1
-                i_g2 += 1
-                continue
-
-        n_match += 1
-        return (Options.excess_coeff * n_excess + Options.disjoint_coeff * n_disjoint) / max(n_g1, n_g2) + Options.weight_coeff * weight_difference / n_match
-
     def same_species(self, brain):
-        return Species.compat_dist(brain, self.best) <= Options.compatibility_threshold
+        return Brain.distance(brain, self.best) <= Options.compatibility_threshold
 
 
 class Population:
